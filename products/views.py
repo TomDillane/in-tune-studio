686c629209aba5c4a3896d8daecada89a38bcc00
TomDillane <tommy.c.dillane@gmail.com>
2021-07-06 20:35:30 +0000

    Product page set up

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def studio_products(request):
+    """ View to return products offered by the studio """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'products/products.html', context)
