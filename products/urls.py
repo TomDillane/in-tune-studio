24377bd15dc321860f59f7d289e2d6ea28ac9ae8
TomDillane <tommy.c.dillane@gmail.com>
2021-07-07 21:11:20 +0000

    Added product detail template and view

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.studio_products, name='products')
+    path('', views.studio_products, name='products'),
+    path('<int:product_id>', views.product_detail, name='product_detail'),
 ]
