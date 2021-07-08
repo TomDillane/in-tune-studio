9f4c25d96b9f125767f6c3c17e9a4e5c1b6b3484
TomDillane <tommy.c.dillane@gmail.com>
2021-07-08 13:30:10 +0000

    Initial cart items form and function

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,15 @@ def view_cart(request):
     """ View to return the cart items """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add items to the cart """
+
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+    
+    cart[item_id] = item_id
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
