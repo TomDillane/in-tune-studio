0a1a33f18cee42a6b4dd8946736e0a1ddd7e9f54
TomDillane <tommy.c.dillane@gmail.com>
2021-07-13 18:11:09 +0000

    added cart items from context

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,19 +11,27 @@ def cart_contents(request):
     total = 0
     date = 0
     count = 0
+    num = 0
     cart = request.session.get('cart', {})
 
     for item_id, date in cart.items():
         product = get_object_or_404(Product, pk=item_id)
         total += product.price
         count = Counter(item_id)
+
+        for k, v in count.items():
+            num = v
+        item_price = product.price * num
+
         # product_count = 3
         cart_items.append({
             'item_id': item_id,
             'date': date,
             'product': product,
-            'count': count,
+            'num': num,
+            'item_price': item_price,
         })
+
 
     grand_total = total
 
