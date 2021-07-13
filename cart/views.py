d45ca9e5d3fae656e80e318ff49727789036ba14
TomDillane <tommy.c.dillane@gmail.com>
2021-07-13 21:19:30 +0000

    updating 'add_to_cart' view to include dates for room

--- cart/views.py
+++ cart/views.py
@@ -13,19 +13,26 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add items to cart """
 
-    
+    bookdate = 'bookdate'
     date = request.POST.get('date')
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
-    # scounter = Counter(cart)
-    cart[item_id] = date
+    # cart[item_id] = date
     
+    if item_id in list(cart.keys()):
+        cart[item_id]['items_by_date'][bookdate] += date
+        # if date in cart[item_id]['items_by_date'].keys():
+            # cart[item_id]['items_by_date'][date] = date
+        # else:
+            # cart[item_id]['items_by_date'][date] = date
+    else:
+        cart[item_id] = {'items_by_date': {'bookdate': date}}
 
     request.session['cart'] = cart
 
-    count = {k: Counter(k) for k, v in request.session['cart'].items()}    
+    # count = {k: Counter(k) for k, v in request.session['cart'].items()}
 
 
     print(request.session['cart'])
-    print(count)
+    # print(count)
     return redirect(redirect_url)
