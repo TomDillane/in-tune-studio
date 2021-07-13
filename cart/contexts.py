b569ed4012fb744fa3aa549c8e6915ec4fda06f9
TomDillane <tommy.c.dillane@gmail.com>
2021-07-13 15:02:51 +0000

    Added contexts.py and included counter on room bookings

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,18 +1,35 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+from collections import Counter
+
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    date = 0
+    count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, date in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += product.price
+        count = Counter(item_id)
+        # product_count = 3
+        cart_items.append({
+            'item_id': item_id,
+            'date': date,
+            'product': product,
+            'count': count,
+        })
 
     grand_total = total
 
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
         'grand_total': grand_total,
     }
 
--- cart/views.py
+++ cart/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render, redirect
+from collections import Counter
 
 # Create your views here.
+
 
 def view_cart(request):
     """ View to return the cart items """
 
     return render(request, 'cart/cart.html')
 
-def add_to_cart(request, item_id):
-    """ Add items to the cart """
 
+def add_to_cart(request, item_id):
+    """ Add items to cart """
+
+    
     date = request.POST.get('date')
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
-    
+    # scounter = Counter(cart)
     cart[item_id] = date
+    
 
     request.session['cart'] = cart
+
+    count = {k: Counter(k) for k, v in request.session['cart'].items()}    
+
+
     print(request.session['cart'])
+    print(count)
     return redirect(redirect_url)
