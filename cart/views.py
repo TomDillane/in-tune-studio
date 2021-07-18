a684404c1afd193387cebf633959602c5fdcfa8a
TomDillane <tommy.c.dillane@gmail.com>
2021-07-18 20:01:49 +0000

    Cleaned up commented out code and started messages

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, reverse
-from collections import Counter
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -14,6 +15,7 @@ def add_to_cart(request, item_id):
     """ Add items to cart """
 
     date = request.POST.get('date')
+    product = get_object_or_404(Product, pk=item_id)
     qty = 0
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -22,29 +24,26 @@ def add_to_cart(request, item_id):
     if item_id in list(cart.keys()):
         if date in cart[item_id]['items_by_date'].values():
             cart[item_id]['items_by_date'][date] = date
+            messages.success(request, f'Added {product.name} to your order!')
 
         else:
             cart[item_id]['items_by_date'][date] = date
+            messages.success(request, f'Added {product.name} to your order!')
     else:
         cart[item_id] = {'items_by_date': {date: date}}
+        messages.success(request, f'Added {product.name} to your order!')
 
     request.session['cart'] = cart
     
 
-    # print(request.session['cart'])
     return redirect(redirect_url)
 
 def remove_from_cart(request, item_id):
     cart = request.session.get('cart', {})
     print("Cart", cart)
-    # print(cart[item_id]['items_by_date'])
-    # print('Request is ', request)
-    # print('item_date is', item_date)
     date = request.POST.get('date-booked')
-    # print(date)
-    # datedict = list(cart[item_id]['items_by_date'].keys())
-
     
+
     if date in cart[item_id]['items_by_date'].values():
         print(date)
         print(cart[item_id]['items_by_date'].values())
@@ -52,80 +51,5 @@ def remove_from_cart(request, item_id):
         print(cart)
 
 
-        """
-        print(cart[item_id]['items_by_date'].values())
-        del dictom[date]
-
-        # del cart[item_id]['items_by_date'][key]
-        
-        """
-
-        """
-        if not cart[item_id]['items_by_date']:
-                    cart.pop(item_id)
-
-    """
-
-
-    """
-    if date in cart[item_id]['items_by_date'].values():
-        del cart[item_id]['items_by_date'][1]
-        if not cart[item_id]['items_by_date']:
-                    cart.pop(item_id)
-    """
-
-
-    # print(cart.items())
-    """
-    for k, v in cart.items():
-        # print(k)
-        # print(v)
-        for g, t in v.items():
-            print(t)
-            if date in t:
-                del cart[item_id]['items_by_date'][date]
-                if not cart[item_id]['items_by_date']:
-                    cart.pop(item_id)
-    """
-    """
-    for date in cart.items():
-        
-        del cart[item_id]['items_by_date'][date]
-        if not cart[item_id]['items_by_date']:
-            cart.pop(item_id)
-    """
-    # del cart[item_id]['items_by_date']['07/15/2021']
-
-
-    """
-    if item_id in list(cart.keys()):
-        for t in cart[item_id]['items_by_date'].values():
-            for k, v in t():
-                print(k)
-    """
-    
-    # for item_id in list(cart.keys()):
-    # for key, date in cart.items():
-        # for a, b in date:
-        # print(date)
-    # for key, date in cart.items():
-        # date = cart[item_id][date]
-        # if date:
-        # del cart[item_id][date]
-        # if not cart[item_id]['items_by_date']:
-            # cart.pop(item_id)
-        
-    
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
-
-
-    
-    """
-    cart = request.session.get('cart', {})
-    date = request.POST.get('date')
-    cart.pop(item_id)
-
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
-    """
