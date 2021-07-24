86ade08b71c4eaf046329ea1e903a84af91ec318
TomDillane <tommy.c.dillane@gmail.com>
2021-07-24 10:36:42 +0000

    Added profile update logic and corrected toast message close function

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import UserProfileForm
+from django.contrib import messages
 
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated!')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
