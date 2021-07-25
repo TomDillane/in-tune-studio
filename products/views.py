6867c5ad5f8f09bd5d1c0ab7cbe012d703dfa019
TomDillane <tommy.c.dillane@gmail.com>
2021-07-25 14:16:25 +0000

    Edit and delete room functions added

--- products/views.py
+++ products/views.py
@@ -34,9 +34,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Room added to studio!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Please review the form detail.')
     else:
@@ -47,3 +47,37 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit details of a room """
+    
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Room updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Please review the form detail.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a room """
+    
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Room removed!')
+    return redirect(reverse('products'))
